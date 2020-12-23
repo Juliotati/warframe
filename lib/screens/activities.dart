7fa369848692ae24680a03ee35e9c80ba5f10a46
juliotati <Juliotati@github.com>
2020-12-23 16:52:13 +0000

    Update activities.dart

--- lib/screens/activities.dart
+++ lib/screens/activities.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:warframe/service/http.dart';
 import 'package:warframe/utilities/scaffold.dart';
-import 'package:warframe/widgets/warframe_drawer.dart';
 
-class ActivitiesScreen extends StatelessWidget {
+class ActivitiesScreen extends StatefulWidget {
   static const route = 'activities_screen';
+
+  @override
+  _ActivitiesScreenState createState() => _ActivitiesScreenState();
+}
+
+class _ActivitiesScreenState extends State<ActivitiesScreen> {
+  WarframeData warframeData = WarframeData();
+
+  @override
+  void initState() {
+    warframeData.getWarframeData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return WarframeScaffold(
       screenName: 'activities',
-      child: Text(
-        'Activity',
-        style: TextStyle(color: Colors.white, fontSize: 32),
+      child: ListView(
+          children: warframeData.warframes
+              .map((e) => DummyWarframe(
+                    name: e.name,
+                  ))
+              .toList()),
+    );
+  }
+}
+
+class DummyWarframe extends StatelessWidget {
+  final String name;
+
+  const DummyWarframe({Key key, this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text(
+          name,
+          style: TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
