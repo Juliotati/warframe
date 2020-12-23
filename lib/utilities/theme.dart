9a9c92ede30f204c4695ec4ecf4741e7d99ea047
juliotati <Juliotati@github.com>
2020-12-23 16:53:29 +0000

    Update theme.dart

--- lib/utilities/theme.dart
+++ lib/utilities/theme.dart
@@ -1,9 +1,15 @@
-
 import 'package:flutter/material.dart';
+import 'package:warframe/utilities/page_transition.dart';
 
 final ThemeData warframeTheme = ThemeData(
+  pageTransitionsTheme: PageTransitionsTheme(
+    builders: <TargetPlatform, PageTransitionsBuilder>{
+      TargetPlatform.android: CustomPageTransitionBuilder(),
+      TargetPlatform.iOS: CustomPageTransitionBuilder(),
+    },
+  ),
   brightness: Brightness.dark,
   accentColor: Colors.black,
   canvasColor: Colors.transparent,
-  appBarTheme: AppBarTheme(color: Colors.transparent,elevation: 0),
+  appBarTheme: AppBarTheme(color: Colors.transparent, elevation: 0),
 );
