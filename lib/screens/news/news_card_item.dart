77935c1fe6ff803999bb8ba08fb74c4e358a8c7d
juliotati <Juliotati@github.com>
2020-12-23 16:53:22 +0000

    Update news_card_item.dart

--- lib/screens/news/news_card_item.dart
+++ lib/screens/news/news_card_item.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:warframe/app_data/news.dart';
+import 'package:warframe/service/news.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-// This widget build the card that display news and news image.
 class NewsCardItem extends StatelessWidget {
-//  final String title;
-//  final String image;
-//  final String webPage;
-//
-//  NewsCardItem(this.title, this.image, this.webPage);
-
   @override
   Widget build(BuildContext context) {
     final newsCard = Provider.of<NewsCard>(context);
-// This launches the url associated with the news report card.
     launchWebPage() async {
       if (await canLaunch(newsCard.webPage)) {
         launch(newsCard.webPage);
