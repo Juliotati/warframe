7e36470702d3e958edebde9a27507791ea3a4648
juliotati <Juliotati@github.com>
2020-12-23 16:52:52 +0000

    Update news.dart

--- lib/screens/news/news.dart
+++ lib/screens/news/news.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:warframe/app_data/news.dart';
+import 'package:warframe/service/news.dart';
 import 'package:warframe/screens/news/news_card_item.dart';
 import 'package:warframe/utilities/scaffold.dart';
 
