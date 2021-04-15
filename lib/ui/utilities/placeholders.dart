71a1a55a700e4479663315f891c50cb9875aa500
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-04-15 22:56:21 +0100

    updated loading indicator

--- lib/ui/utilities/placeholders.dart
+++ lib/ui/utilities/placeholders.dart
@@ -26,7 +26,9 @@ class LoadingIndicator extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const CircularProgressIndicator(backgroundColor: Colors.white),
+        const CircularProgressIndicator(
+          backgroundColor: Color.fromRGBO(255, 255, 255, 0.2),
+        ),
         const SizedBox(height: 15),
         Text(label ?? 'GETTING GAME DATA'),
       ],
