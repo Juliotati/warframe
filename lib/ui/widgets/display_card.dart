2e5201f1f30b440e0bdef4ace504b085de4f9dda
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-04-16 17:33:19 +0100

    created a DisplayCard

---
+++ lib/ui/widgets/display_card.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DisplayCard extends StatelessWidget {
+  const DisplayCard({this.child, this.onTap});
+
+  final Widget child;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        elevation: 20.0,
+        shadowColor: const Color.fromRGBO(255, 255, 255, 0.1),
+        color: const Color.fromRGBO(255, 255, 255, 0.3),
+        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        child: child,
+      ),
+    );
+  }
+}
