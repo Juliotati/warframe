8ab0026304bcd8a969d28f14bddbe9aa6d4f591c
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-04-16 17:50:16 +0100

    releted activities export

--- lib/export/warframe_ui.dart
+++ lib/export/warframe_ui.dart
@@ -1,8 +1,7 @@
 export 'package:warframe/ui/utilities/theme.dart';
 export 'package:warframe/ui/widgets/drawer/drawer_data.dart';
-export 'package:warframe/ui/screens/activities.dart';
 export 'package:warframe/ui/screens/codex_categories.dart';
-export 'package:warframe/ui/screens/codex_grid.dart';
+export 'package:warframe/ui/screens/codex_category_data.dart';
 export 'package:warframe/ui/screens/warframe_profile.dart';
 export 'package:warframe/ui/screens/login.dart';
 export 'package:warframe/ui/screens/news.dart';
