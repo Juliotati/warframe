bd317fdeb53ae882299cfce63266162528d71aac
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-04-16 17:36:22 +0100

    updated exports

--- lib/export/warframe_ui.dart
+++ lib/export/warframe_ui.dart
@@ -1,7 +1,7 @@
 export 'package:warframe/ui/utilities/theme.dart';
 export 'package:warframe/ui/widgets/drawer/drawer_data.dart';
 export 'package:warframe/ui/screens/activities.dart';
-export 'package:warframe/ui/screens/codex.dart';
+export 'package:warframe/ui/screens/codex_categories.dart';
 export 'package:warframe/ui/screens/codex_grid.dart';
 export 'package:warframe/ui/screens/warframe_profile.dart';
 export 'package:warframe/ui/screens/login.dart';
