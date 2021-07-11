326ad5e279ac005d5317c48f50d0617c9f7a1f87
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-07-11 17:14:13 +0100

    Update codex_data_scaffold.dart

--- lib/features/warframe_codex/presentation/widgets/codex_data_scaffold.dart
+++ lib/features/warframe_codex/presentation/widgets/codex_data_scaffold.dart
@@ -6,10 +6,12 @@ class CodexDataScaffold extends StatelessWidget {
     Key? key,
     required this.label,
     required this.body,
+    this.icon,
   }) : super(key: key);
 
   final String label;
   final Widget body;
+  final IconData? icon;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,30 +19,51 @@ class CodexDataScaffold extends StatelessWidget {
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        leading: WarframeContainer(
-          horizontalPadding: 0.0,
-          verticalPadding: 0.0,
-          margin: 9.0,
-          centerChild: true,
-          color: const Color.fromRGBO(255, 255, 255, 0.6),
-          onTap: () => Navigator.of(context).pop(),
-          child: const Icon(
-            Icons.arrow_back,
-            color: Color.fromRGBO(158, 158, 158, 1.0),
+        leading: Hero(
+          tag: 'app-bar-icon',
+          child: Material(
+            child: WarframeContainer(
+              horizontalPadding: 0.0,
+              verticalPadding: 0.0,
+              margin: 9.0,
+              centerChild: true,
+              color: const Color.fromRGBO(255, 255, 255, 0.6),
+              onTap: () => Navigator.of(context).pop(),
+              child:   Icon(
+                icon ?? Icons.arrow_back,
+                color: const Color.fromRGBO(255, 255, 255, 1.0),
+              ),
+            ),
           ),
         ),
         titleSpacing: 0.0,
-        title: WarframeContainer(
-          child: Text(
-            label,
-            style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                  fontSize: 18,
-                  color: const Color.fromRGBO(0, 0, 0, 1.0),
-                ),
+        title: Hero(
+          tag: 'app-bar-title',
+          child: Material(
+            child: WarframeContainer(
+              child: Text(
+                label,
+                style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      fontSize: 24,
+                      color: const Color.fromRGBO(0, 0, 0, 1.0),
+                    ),
+              ),
+            ),
           ),
         ),
       ),
-      body: body,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          image: DecorationImage(
+            image: AssetImage(kBackgroundShip),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: body,
+      ),
     );
   }
 }
