5426f8cc2ebe4815a7feaf5dbf8feddb49829221
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-07-11 17:14:46 +0100

    Update wrapper.dart

--- lib/core/presentation/wrapper.dart
+++ lib/core/presentation/wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:provider/provider.dart';
+import 'package:warframe/features/warframe_news/data/datasources/news_network.dart';
 import '../../export/warframe_ui.dart';
 import '../../features/warframe_codex/presentation/screens/codex_categories.dart';
 import '../../features/warframe_news/presentation/screens/news.dart';
@@ -12,51 +13,56 @@ class WarframeWrapper extends StatelessWidget {
     final LayoutHelper layout = context.watch<LayoutHelper>();
     final bool isNewsScreen = layout.screen == Screen.news;
 
-    return Scaffold(
-      drawerEdgeDragWidth: 50,
-      extendBody: true,
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.transparent,
-      drawer: const WarframeDrawer(),
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        title: Text(layout.label.toUpperCase()),
-        actions: <WarframeContainer>[
-          const WarframeContainer(
-            height: 35,
-            width: 35,
-            margin: 10.0,
-            color: Color.fromRGBO(255, 255, 255, 0.4),
-            centerChild: true,
-            horizontalPadding: 0.0,
-            verticalPadding: 0.0,
-            child: Icon(Icons.group),
-          ),
-          WarframeContainer(
-            height: 35,
-            width: 35,
-            margin: 10.0,
-            color: const Color.fromRGBO(255, 255, 255, 0.4),
-            centerChild: true,
-            horizontalPadding: 0.0,
-            verticalPadding: 0.0,
-            child: Icon(isNewsScreen ? Icons.refresh : Icons.chat_bubble),
-          ),
-        ],
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          image: DecorationImage(
-            image: AssetImage('images/ship.jpg'),
-            fit: BoxFit.cover,
-          ),
+    return WillPopScope(
+      onWillPop: () {
+        return Future<bool>.value(false);
+      },
+      child: Scaffold(
+        drawerEdgeDragWidth: 50,
+        extendBody: true,
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.transparent,
+        drawer: const WarframeDrawer(),
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          title: Text(layout.label.toUpperCase()),
+          actions: <WarframeContainer>[
+            const WarframeContainer(
+              height: 35,
+              width: 35,
+              margin: 10.0,
+              color: Color.fromRGBO(255, 255, 255, 0.4),
+              centerChild: true,
+              horizontalPadding: 0.0,
+              verticalPadding: 0.0,
+              child: Icon(Icons.group),
+            ),
+            WarframeContainer(
+              height: 35,
+              width: 35,
+              margin: 10.0,
+              color: const Color.fromRGBO(255, 255, 255, 0.4),
+              centerChild: true,
+              horizontalPadding: 0.0,
+              verticalPadding: 0.0,
+              child: Icon(isNewsScreen ? Icons.refresh : Icons.chat_bubble),
+            ),
+          ],
         ),
-        child: Center(
-          child: layout.currentScreen(),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            image: DecorationImage(
+              image: AssetImage(kBackgroundShip),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: layout.currentScreen(),
+          ),
         ),
       ),
     );
