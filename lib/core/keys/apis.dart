912b6655876b85f7140772a530778fc9bffb2828
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-07-24 20:28:37 +0100

    created missing files

---
+++ lib/core/keys/apis.dart
@@ -0,0 +1,7 @@
+class API {
+  static const String baseUrl = 'https://api.warframestat.us';
+  static const String warframeAPI = '$baseUrl/warfames';
+  static const String weaponsAPI = '$baseUrl/ps4/news';
+  static const String newsAPI = '$baseUrl/weapons/';
+  static const String modsAPI = '$baseUrl/mods';
+}
