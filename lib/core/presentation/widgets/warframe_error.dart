330b4a9597ea52ffc54093810f64b5e9c0fa9001
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-09-24 12:30:32 +0100

    update error default text

--- lib/core/presentation/widgets/warframe_error.dart
+++ lib/core/presentation/widgets/warframe_error.dart
@@ -1,14 +1,20 @@
 part of presentation;
 
 class WarframeError extends StatelessWidget {
-  const WarframeError([this.message]);
+  const WarframeError([this.message = 'NO DATA AVAILABLE']);
 
-  final String? message;
+  final String message;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(message ?? 'Found no warframes'),
+      child: Text(
+        message,
+        style: const TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
