93b4c6048a653714ee86d0f0653491d4fe067496
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-09-29 00:44:09 +0100

    add class for dead links

---
+++ lib/core/utilities/dead_links.dart
@@ -0,0 +1,7 @@
+class DeadLink {
+  static const String baseUrl = 'https://dev.warframe.com';
+  static const List<String> links = <String>[
+    'https://dev.warframe.com/',
+    'https://dev.warframe.com/uploads/ec5055bcdcb45a5b0ad2ae63870e1c6d.jpg'
+  ];
+}
