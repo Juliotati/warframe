ebea6d3de6788d52bb4691329f6b0ad70a1b0bf8
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-10-01 16:40:14 +0100

    configure network class

--- lib/core/platform/network_info.dart
+++ lib/core/platform/network_info.dart
@@ -1,3 +1,39 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetWorkInfoImpl implements NetworkInfo {
+  NetWorkInfoImpl._();
+
+  static NetWorkInfoImpl instance = NetWorkInfoImpl._();
+
+  @override
+  Future<bool> get isConnected => _isConnected();
+
+  Future<bool> _isConnected() async {
+    if (await _hasNoNetwork()) return false;
+    if (await _hasNetwork()) return true;
+    return false;
+  }
+
+  final Future<ConnectivityResult> _result = Connectivity().checkConnectivity();
+
+  Future<bool> _hasNetwork() async {
+    final ConnectivityResult result = await _result;
+    return result == ConnectivityResult.mobile ||
+        result == ConnectivityResult.wifi;
+  }
+
+  Future<bool> _hasNoNetwork() async {
+    final ConnectivityResult result = await _result;
+    return result == ConnectivityResult.none || !(await _hasNetwork());
+  }
+
+  Future<Stream<ConnectivityResult>> connectionChanged() async {
+    return Connectivity().onConnectivityChanged;
+  }
 }
