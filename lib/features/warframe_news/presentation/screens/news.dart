8e1dffd5ab98f0e84ccf9df5aaa0dbf0f1290aa8
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-10-02 14:11:33 +0100

    add key to news widget

--- lib/features/warframe_news/presentation/screens/news.dart
+++ lib/features/warframe_news/presentation/screens/news.dart
@@ -9,6 +9,7 @@ class NewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size screen = MediaQuery.of(context).size;
     return SafeArea(
+      key: const ValueKey<String>('warframe-news-screen'),
       child: SizedBox(
         height: screen.height,
         width: screen.width,
