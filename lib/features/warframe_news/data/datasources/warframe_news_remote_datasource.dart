f79f9b1d608879d160e23b02bb291aa33d426d13
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-10-04 20:41:05 +0100

    Added documentation and better abstraction

--- lib/features/warframe_news/data/datasources/warframe_news_remote_datasource.dart
+++ lib/features/warframe_news/data/datasources/warframe_news_remote_datasource.dart
@@ -1,14 +1,17 @@
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:warframe/core/helpers/datasource_helper.dart';
 import 'package:warframe/core/keys/apis.dart';
+import 'package:warframe/core/platform/network_info.dart';
 
 import '../models/warframe_news.dart';
 
 abstract class WarframeNewsRemoteDatasource {
+
+  /// Get the latest news from the API about the warframe game or community.
   Future<void> getRemoteWarframeNews();
 
+  /// Refresh news data in the app to get updated news if there happens to be.
   Future<void> refresh();
 }
 
@@ -23,14 +26,37 @@ class WarframeNewsRemoteDatasourceImpl extends WarframeNewsRemoteDatasource with
 
   NewsState state = NewsState.loading;
 
-  int _retryCount = 0;
+  static int _retryCount = 0;
   static const int _thresholdLimit = 5;
 
   @override
   Future<void> getRemoteWarframeNews() async {
-    final http.Response response = await http.get(Uri.parse(API.newsAPI));
-    final List<dynamic> _decodedData = await jsonDecode(response.body) as List<dynamic>;
 
+    /// Get connection state form NetWorkInfoImpl class
+    final bool isConnected = await NetWorkInfoImpl.instance.isConnected;
+
+    /// Check whether the device has connection or not.
+    /// If no connection is detected, the method is existed.
+    if (!isConnected) {
+      _endProcessWithEmptyState();
+      return;
+    }
+
+    final http.Response response = await DatasourceHelper.get(API.newsAPI);
+
+    if (response.statusCode != 200) {
+      _endProcessWithEmptyState();
+      return;
+    }
+
+    /// Decode the response body from the HTTP GET
+    final List<dynamic> _decodedData = await DatasourceHelper.decodeData(response.body);
+
+    /// If _decodedData list comes in empty, the method whole method should
+    /// re-run.
+    ///
+    /// If the there happens to be many tries after re-running the method whole
+    /// will exits too.
     if (_decodedData.isEmpty) {
       if (_timedOut()) {
         _endProcessWithEmptyState();
@@ -43,47 +69,61 @@ class WarframeNewsRemoteDatasourceImpl extends WarframeNewsRemoteDatasource with
       return;
     }
 
-    final List<WarframeNewsModel> newsList = await _newsList(_decodedData);
-    data ??= newsList;
+    List<WarframeNewsModel>? _news = await _newsList(_decodedData);
+
+    if (data == null) {
+      data = _news;
+      _endProcessWithLoadedState();
+      return;
+    }
 
     if (data!.isNotEmpty) {
-      await _addNewData(newsList);
+      await _addNewData(_news);
+      _news = null;
     }
-    state = NewsState.loaded;
-    notifyListeners();
+
+    _endProcessWithLoadedState();
   }
 
+  /// Should return whether the method has ran out of tries count or not
+  ///
+  /// Everytime [getRemoteWarframeNews] re-runs, [_retryCount] increments, if
+  /// [_retryCount] happens to be equal to or, exceed [_thresholdLimit] the
+  /// method call should exit to avoid infinity loops
   bool _timedOut() {
     return _retryCount >= _thresholdLimit;
   }
 
-  void _endProcessWithEmptyState(){
+  /// Called when [getRemoteWarframeNews] is exiting which, was unsuccessful and
+  /// [NewsState] needs/has to be set to loaded state
+  void _endProcessWithEmptyState() {
     state = NewsState.empty;
     notifyListeners();
   }
 
+  /// Called when [getRemoteWarframeNews] is exiting which, was successful and
+  /// [NewsState] needs/has to be set to loaded state
+  void _endProcessWithLoadedState() {
+    state = NewsState.loaded;
+    notifyListeners();
+  }
+
+  /// Transform the decoded data into dart objects as [WarframeNewsModel]
   Future<List<WarframeNewsModel>> _newsList(List<dynamic> data) async {
-    return data.map((dynamic news) {
-      return WarframeNewsModel.fromJson(news as Map<String, dynamic>);
-    }).toList().reversed.toList();
+    return data
+        .map((dynamic news) {
+          return WarframeNewsModel.fromJson(news as Map<String, dynamic>);
+        })
+        .toList()
+        .reversed
+        .toList();
   }
 
   Future<void> _addNewData(List<WarframeNewsModel> newsList) async {
-     for (final WarframeNewsModel news in newsList) {
-      if (!_itemExist(news)) data!.insert(0, news);
-    }
-  }
-
-  bool _itemExist(WarframeNewsModel news) {
-    try {
-      final WarframeNewsModel existingNews =
-          data!.firstWhere((WarframeNewsModel element) {
-        return element.id == news.id;
-      });
-      if (existingNews.id == news.id) return true;
-      return false;
-    } catch (_) {
-      return false;
+    for (final WarframeNewsModel _item in newsList) {
+      if (!DatasourceHelper.idExists(data!, _item)) {
+        data!.insert(0, _item);
+      }
     }
   }
 
